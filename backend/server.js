c038176fc8cf0bb5c59a66a7ea5fc3514b523225
Hari Om Kumar Jha <hariomkumar6april2014@gmail.com>
2022-09-10 10:46:34 +0530

    sec

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,17 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+/*
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+*/
+
 
 app.use(notFound);
 
